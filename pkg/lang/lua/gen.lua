7420859c76ac53f1693944921719ad11b0bc2830
Randy Palamar <palamar@ualberta.ca>
2023-10-14 08:14:07 -0600

    lua: upgrade back to lua 5.2.4
    This reverts commit d1063e30adbae693253ea169c8daeab5fbd80bbc.
    
    Apparently vis still works with 5.2 (it was in issue with and in
    progress update) and 5.2 is much smaller. No need to use 5.4 when
    other packages like mpv need 5.2 or lower.

--- pkg/lang/lua/gen.lua
+++ pkg/lang/lua/gen.lua
@@ -13,14 +13,14 @@ lib('liblua.a', [[src/(
 	lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c
 	lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c
 	ltm.c lundump.c lvm.c lzio.c
-	lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c
-	loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c
+	lauxlib.c lbaselib.c lbitlib.c lcorolib.c ldblib.c liolib.c
+	lmathlib.c loslib.c lstrlib.c ltablib.c loadlib.c linit.c
 )]])
 file('lib/liblua.a', '644', '$outdir/liblua.a')
 
 exe('lua', {'src/lua.c', 'liblua.a'})
-file('bin/lua5.4', '755', '$outdir/lua')
-sym('bin/lua', 'lua5.4')
+file('bin/lua5.2', '755', '$outdir/lua')
+sym('bin/lua', 'lua5.2')
 man({'doc/lua.1'})
 
 fetch('curl')
