adaef83f6e14f853664c99788ff6951ae820b45c
Randy Palamar <randy@rnpnr.xyz>
2024-03-09 15:20:55 -0700

    libpng: bump to 1.6.43

--- pkg/libs/libpng/config.h
+++ pkg/libs/libpng/config.h
@@ -16,10 +16,10 @@
 #define PACKAGE "libpng"
 #define PACKAGE_BUGREPORT "png-mng-implement@lists.sourceforge.net"
 #define PACKAGE_NAME "libpng"
-#define PACKAGE_STRING "libpng 1.6.42"
+#define PACKAGE_STRING "libpng 1.6.43"
 #define PACKAGE_TARNAME "libpng"
 #define PACKAGE_URL ""
-#define PACKAGE_VERSION "1.6.42"
+#define PACKAGE_VERSION "1.6.43"
 /* #undef PNG_ARM_NEON_API_SUPPORTED */
 /* #undef PNG_ARM_NEON_CHECK_SUPPORTED */
 /* #undef PNG_ARM_NEON_OPT */
@@ -36,7 +36,7 @@
 /* #undef PNG_POWERPC_VSX_OPT */
 #define STDC_HEADERS 1
 /* #undef TM_IN_SYS_TIME */
-#define VERSION "1.6.42"
+#define VERSION "1.6.43"
 #define restrict __restrict__
 #if defined __SUNPRO_CC && !defined __RESTRICT && !defined __restrict__
 # define _Restrict
