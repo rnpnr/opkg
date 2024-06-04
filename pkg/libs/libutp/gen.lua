d63e62ca4c3150eb1f8ee3ceb441432abd13e2cc
Randy Palamar <randy@rnpnr.xyz>
2024-06-04 05:43:13 -0600

    add libutp from oasis

---
+++ pkg/libs/libutp/gen.lua
@@ -0,0 +1,11 @@
+cflags({
+	'-std=c99', '-Wall', '-Wpedantic',
+	'-D _POSIX_C_SOURCE=200809L',
+	'-I $srcdir',
+})
+
+pkg.hdrs = copy('$outdir/include/libutp', '$srcdir', {'utp.h'})
+
+lib('libutp.a', {'utp.c', 'utp_utils.c'})
+
+fetch('git')
