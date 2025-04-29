46f89c933f1ae90934d9dd311372acdcc884236a
Randy Palamar <randy@rnpnr.xyz>
2025-04-28 20:47:53 -0600

    pull pax in from oasis

---
+++ pkg/arch/pax/gen.lua
@@ -0,0 +1,12 @@
+cflags({'-std=c99', '-Wall', '-Wpedantic'})
+
+exe('pax', {'pax.c'})
+file('bin/pax', '755', '$outdir/pax')
+
+sub('host.ninja', function()
+	set('outdir', '$outdir/host')
+	toolchain(config.host)
+	exe('pax', {'pax.c'})
+end)
+
+fetch('git')
